01e1d70ab0771f359bbb2f0d1602ddf17dbc8eaf
tianduo123 <954026506@qq.com>
2019-01-29 17:32:22 +0800

    2019.1.29.2

--- pages/me/me.js
+++ pages/me/me.js
@@ -15,27 +15,40 @@ Page({
    */
   onLoad: function (options) {
     console.log(options)
+    console.log(app.globalData.userId)
+
   },
   //积分兑换
   jifen(){
-    //判断用户是否登录、
-    wx.request({
-      url: api.isLogin(app.globalData.openid),
-      success:(res)=>{
-        console.log(res)
-        if(res.data.status==0){
-          //用户未注册，跳转注册界面
-          wx.navigateTo({
-            url: '../login/login',
-          })
-        }else{
-          //用户已注册，跳转登录界面
-          wx.reLaunch({
-            url: '../register/register',
-          })
+    //判断用户是否登录，登录-->跳转积分兑换页面。未登录-->判断用户是否注册
+    //从app.js中拿用户id用于判断用于判断用户是否登录
+    console.log(app.globalData.userId)
+    if(app.globalData.userId){
+      //已登录 --> 跳转积分兑换页面
+      wx.navigateTo({
+        url: '../near/near',
+      })
+    }else{
+      //未登录 --> 判断用户是否注册
+      //判断用户是否注册
+      wx.request({
+        url: api.isLogin(app.globalData.openid),
+        success: (res) => {
+          console.log(res)
+          if (res.data.status == 0) {
+            //用户未注册，跳转注册界面
+            wx.navigateTo({
+              url: '../login/login',
+            })
+          } else {
+            //用户已注册，跳转登录界面
+            wx.reLaunch({
+              url: '../register/register',
+            })
+          }
         }
-      }
-    })
+      })
+    }
   },
   //每日签到
   qiandao(){
--- pages/register/register.js
+++ pages/register/register.js
@@ -13,7 +13,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     //从缓存中拿用户微信头像
     wx.getStorage({
       key: 'userInfo',
@@ -26,38 +26,40 @@ Page({
     })
   },
   //登录
-  register(e){
+  register(e) {
     console.log(e)
     wx.request({
-      url: api.register(e.detail.value.phone,e.detail.value.password),
-      success:(res)=>{
+      url: api.register(e.detail.value.phone, e.detail.value.password),
+      success: (res) => {
         console.log(res)
-        wx.setStorage({
-          key: 'userId',
-          data: res.data.data.id,
-        })
-        wx.setStorage({
-          key: 'jifen',
-          data: res.data.data.score,
-        })
-        if(res.data.status == 0){
+
+        if (res.data.status == 0) {
           //失败
           wx.showToast({
             title: '手机号或密码输入错误',
-            icon:'none'
+            icon: 'none'
           })
-        }else{
-          //成功
+        } else {
+          //成功,将用户id和积分存到缓存（用于判断用户是否登录）
+          app.globalData.userId = res.data.data.id
+          wx.setStorage({
+            key: 'userId',
+            data: res.data.data.id,
+          })
+          wx.setStorage({
+            key: 'jifen',
+            data: res.data.data.score,
+          })
           wx.showToast({
             title: '登录成功',
-            success:()=>{
-              setTimeout(()=>{
+            success: () => {
+              setTimeout(() => {
                 wx.switchTab({
                   url: '../me/me',
                 })
-              },1500)
+              }, 1500)
             }
-          })         
+          })
         }
       }
     })
@@ -65,49 +67,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
